c65ebe8b9c77b6834164668af7d674505c6401db
hamlim <matthamlin@Neil-Hamlins-MacBook-Pro.local>
2016-01-04 17:06:47 -0800

    attempting to implement dynamic status page

--- Assets/js/status.js
+++ Assets/js/status.js
@@ -10,4 +10,29 @@ $(document).ready(function(){
     var day = Math.floor(diff / oneDay);
     var d = day.toString();
     progressBar.value = d;
+
+
+    // var mustachAttachPoint = document.getElementById('mustache-attach')
+    // //dynamic loading of status updates
+    // var templateSettings = {
+    //     "async": true,
+    //     "url": "./status.mst",
+    //     "method": "GET"
+    // }
+    // var dataSettings = {
+    //     "async",
+    //     "url": "./status.json",
+    //     "method": "GET"
+    // }
+    // $.ajax(dataSettings).done(function(response){
+    //     $.ajax(templateSettings).done(function(resp){
+    //         var template = resp;
+    //         var rendered = Mustache.render(template, response);
+    //         mustachAttachPoint.innerHTML = rendered;
+    //     });
+    //
+    //
+    // });
+
+
 });
