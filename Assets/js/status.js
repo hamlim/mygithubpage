451cf7f28a61075e72f55bf0da4d64736d17e39a
hamlim <hamlim@rpi.edu>
2016-03-16 14:10:43 -0400

    final update to status

--- Assets/js/status.js
+++ Assets/js/status.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     //document is loaded
     //get the progress bar
-    var progressBar = document.getElementById('progress-bar');
-    //get current date
-    var now = new Date();
-    var start = new Date(now.getFullYear(), 0, 0);
-    var diff = now - start;
-    var oneDay = 1000 * 60 * 60 * 24;
-    var day = Math.floor(diff / oneDay);
-    var d = day.toString();
-    progressBar.value = d;
+    // var progressBar = document.getElementById('progress-bar');
+    // //get current date
+    // var now = new Date();
+    // var start = new Date(now.getFullYear(), 0, 0);
+    // var diff = now - start;
+    // var oneDay = 1000 * 60 * 60 * 24;
+    // var day = Math.floor(diff / oneDay);
+    // var d = day.toString();
+    // progressBar.value = d;
 
 
     // var mustachAttachPoint = document.getElementById('mustache-attach')
