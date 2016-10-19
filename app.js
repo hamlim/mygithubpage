a1beac1ce75d145d8563ab6b7fbbdee7c926a671
Matt Hamlin <hamlim@outlook.com>
2016-10-19 18:46:06 -0400

    Webpack and React

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Nav } from './assets/js/src/components';
+var css = require('./assets/css/main.css');
+
+ReactDOM.render(<Nav />, document.getElementById('navMount'));
